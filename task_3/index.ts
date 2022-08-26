271ec628304cd2d0d5f470b175bf8c3028b7ff3c
Egorsemigor <egorsemigor@gmail.com>
2022-08-26 17:29:31 +0300

    hw3

--- task_3/index.ts
+++ task_3/index.ts
@@ -1,6 +1,6 @@
 // ============
 
-// #### Задачи: 
+// #### Задачи:
 
 // С ниже приведенным массивом решить следующие задачи. Все функции и данные должны быть протипизированы:
 
@@ -10,69 +10,72 @@
 //     4. Создать функцию, которая бы принимала массив пользователей и отфильтровывала пользователей на наличие животных
 //     5. Создать функцию, которая бы принимала массив пользователей и отдавала бы  строку с названиями марок автомобилей через запятую
 
-interface User{
-    name: string,
-    phone: string,
-    email: string,
-    animals?: string[],
-    cars?: string[],
-    hasChildren: boolean,
-    hasEducation: boolean
+interface User {
+  name: string;
+  phone: string;
+  email: string;
+  animals?: string[];
+  cars?: string[];
+  hasChildren: boolean;
+  hasEducation: boolean;
 }
 const users: User[] = [
-{
+  {
     name: "Harry Felton",
     phone: "(09) 897 33 33",
     email: "felton@gmail.com",
     animals: ["cat"],
     cars: ["bmw"],
     hasChildren: false,
-    hasEducation: true
-    
-},
-{
+    hasEducation: true,
+  },
+  {
     name: "May Sender",
     phone: "(09) 117 33 33",
     email: "sender22@gmail.com",
     hasChildren: true,
-    hasEducation: true
-},
-{
+    hasEducation: true,
+  },
+  {
     name: "Henry Ford",
     phone: "(09) 999 93 23",
     email: "ford0@gmail.com",
     cars: ["bmw", "audi"],
     hasChildren: true,
-    hasEducation: false
-}
-]
+    hasEducation: false,
+  },
+];
 
 //task1
 
-const task1 = Object.values(users).map(item => item.name).join(', ');
+const task1 = users.map((item) => item.name).join(", ");
 console.log(task1);
 
-
 //task2
-const task2 = Object.values(users).filter(el => el.cars).map(item => item.cars).flat().length;
-console.log(task2)
-
+const task2 = users
+  .filter((el) => el.cars)
+  .map((item) => item.cars)
+  .flat().length;
+console.log(task2);
 
 //task3
-function usersEducation(users:User[]) {
-    return users.filter(el => el.hasEducation);
+function usersEducation(users: User[]) {
+  return users.filter((el) => el.hasEducation);
 }
 console.log(usersEducation(users));
 
-
 //task4
-function usersAnimals(users:User[]) {
-    return users.filter(el => el.animals);
+function usersAnimals(users: User[]) {
+  return users.filter((el) => el.animals);
 }
-console.log(usersAnimals(users))
+console.log(usersAnimals(users));
 
 //task5
-function carsNames(users:User[]) {
-return users.filter(el => el.cars).map(item => item.cars).flat().join(', ');
-} 
+function carsNames(users: User[]) {
+  return users
+    .filter((el) => el.cars)
+    .map((item) => item.cars)
+    .flat()
+    .join(", ");
+}
 console.log(carsNames(users));
