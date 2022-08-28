2c21fb6c69d4aeed7c2c050ccb094c9d22db685a
Egorsemigor <egorsemigor@gmail.com>
2022-08-28 11:28:04 +0300

    hw2

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Button } from "./components/Button";
 import "./App.css";
 import { Input } from "./components/Input";
@@ -8,25 +8,15 @@ import { Clicker } from "./components/Clicker";
 import { EmojiList } from "./components/emojiList";
 import { PostList } from "./components/ToDoList/PostList";
 
-function App() {
-  const onClickLogin = () => {
-    alert("Login");
-=======
-import {Button} from './components/Button'
-import './App.css';
-import { Input } from './components/Input'
-import { Title } from './components/Title';
-import { Username } from './components/User'
+
 
 function App() {
   const onClickLogin = () => {
     alert('Login')
->>>>>>> 42d188ce897cd37ac684fd3baef20a7f9d518dda
   };
 
   return (
     <div className="App">
-<<<<<<< HEAD
       {/* <Button onClick={onClickLogin} text="LogIn" />
       <Input value={"Enter your name"} />
       <Title text="Text" />
@@ -38,7 +28,7 @@ function App() {
       <PostList />
       <Clicker />
       <EmojiList />
-=======
+
       <Button onClick = {onClickLogin} text = "LogIn"/>
       <Input value={'Enter your name'}/>
       <Title text='Text'/>
@@ -47,7 +37,7 @@ function App() {
       <Username username={'John Newman'}/>
       <Username username={'Вася Пупкин'}/>
       <Username username={'Джон Пупс'}/>
->>>>>>> 42d188ce897cd37ac684fd3baef20a7f9d518dda
+
     </div>
   );
 }
